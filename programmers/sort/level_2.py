5e7f84030d3400aa9962333fa7554eb6bc979e36
Ryumyunggi <fbaudrl96@soulinno.com>
2022-01-10 16:33:50 +0900

    create programmers directory * sort level2 complete

---
+++ programmers/sort/level_2.py
@@ -0,0 +1,36 @@
+"""
+문제 설명
+0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+
+예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다.
+
+0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+
+제한 사항
+numbers의 길이는 1 이상 100,000 이하입니다.
+numbers의 원소는 0 이상 1,000 이하입니다.
+정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
+입출력 예
+numbers	return
+[6, 10, 2]	"6210"
+[3, 30, 34, 5, 9]	"9534330"
+"""
+# my solution
+def solution(numbers):
+    answer = ''
+    numbers = list(map(lambda el: str(el), numbers))
+    numbers.sort(key=lambda el: el * 3, reverse=True)
+    for number in numbers:
+        answer += str(number)
+
+    return str(int(answer))
+
+# programmers best solution
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
+
+
+if __name__ == '__main__':
+    print(solution([6, 10, 2]))
