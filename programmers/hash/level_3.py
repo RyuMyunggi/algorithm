1ac93a8c3be35cca14852a58f25c087819ece446
Ryumyunggi <fbaudrl96@soulinno.com>
2022-01-12 11:18:29 +0900

    create programmers directory * hash level 3 complete

---
+++ programmers/hash/level_3.py
@@ -0,0 +1,81 @@
+"""
+
+문제 설명
+스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
+
+속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
+
+제한사항
+genres[i]는 고유번호가 i인 노래의 장르입니다.
+plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
+genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하입니다.
+장르 종류는 100개 미만입니다.
+장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
+모든 장르는 재생된 횟수가 다릅니다.
+입출력 예
+genres	plays	return
+["classic", "pop", "classic", "classic", "pop"]	[500, 600, 150, 800, 2500]	[4, 1, 3, 0]
+입출력 예 설명
+classic 장르는 1,450회 재생되었으며, classic 노래는 다음과 같습니다.
+
+고유 번호 3: 800회 재생
+고유 번호 0: 500회 재생
+고유 번호 2: 150회 재생
+pop 장르는 3,100회 재생되었으며, pop 노래는 다음과 같습니다.
+
+고유 번호 4: 2,500회 재생
+고유 번호 1: 600회 재생
+따라서 pop 장르의 [4, 1]번 노래를 먼저, classic 장르의 [3, 0]번 노래를 그다음에 수록합니다.
+
+※ 공지 - 2019년 2월 28일 테스트케이스가 추가되었습니다.
+"""
+
+
+# my solution
+def solution(genres, plays):
+    answer = []
+
+    genre_obj = {}
+    for i in range(len(genres)):
+        genre = genres[i]
+        genre_obj.setdefault(genre, {
+            'plays': [],
+            'total_play': 0
+        })
+        genre = genres[i]
+        play = plays[i]
+        genre_obj[genre]['plays'].append(play)
+        genre_obj[genre]['total_play'] += play
+
+    sorted_plays = sorted(genre_obj.items(), key=lambda el: el[1].get('total_play'), reverse=True)
+
+    for tu in sorted_plays:
+        genre_plays = tu[1].get('plays')
+        genre_plays.sort(reverse=True)
+        for play in genre_plays[:2]:
+            indexes = [i for i in range(len(plays)) if plays[i]==play]
+            if len(indexes) >= 2:
+                answer.extend(indexes[:2])
+                break
+            else:
+                answer.extend(indexes[:2])
+    return answer
+
+
+# programmers best solution
+def solution(genres, plays):
+    answer = []
+    d = {e:[] for e in set(genres)}
+    for e in zip(genres, plays, range(len(plays))):
+        d[e[0]].append([e[1] , e[2]])
+    genreSort =sorted(list(d.keys()), key= lambda x: sum( map(lambda y: y[0],d[x])), reverse = True)
+    for g in genreSort:
+        temp = [e[1] for e in sorted(d[g],key= lambda x: (x[0], -x[1]), reverse = True)]
+        answer += temp[:min(len(temp),2)]
+    return answer
+
+if __name__ == '__main__':
+    print(solution(['A', 'A', 'B', 'A'], [5, 5, 6, 5]))
